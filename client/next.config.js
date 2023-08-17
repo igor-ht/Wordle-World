2b36060b105e0db6524e185b0a3f4ea3e3d410d6
igor-ht <i_ht93@hotmail.com>
2023-08-17 13:34:48 +0300

    google-site-verification + x-robots-tag set for indexing

--- client/next.config.js
+++ client/next.config.js
@@ -2,15 +2,17 @@
 
 const nextConfig = {
 	async headers() {
-		return {
-			headers: [
-				{
-					key: 'X-Robots-Tag',
-					value: process.env.NEXT_PUBLIC_VERCEL_ENV === 'production' ? 'all' : 'noindex',
-				},
-			],
-			source: '/:path*',
-		};
+		return [
+			{
+				headers: [
+					{
+						key: 'X-Robots-Tag',
+						value: process.env.NEXT_PUBLIC_VERCEL_ENV === 'production' ? 'all' : 'noindex',
+					},
+				],
+				source: '/:path*',
+			},
+		];
 	},
 };
 
