77c6b90cf2e439ab81d723f1d21f0b5e2831df19
igor-ht <i_ht93@hotmail.com>
2024-01-28 20:18:35 +0200

    react-query client moved to provider file

--- client/src/app/_components/providers/ReactQueryProvider/ReactQueryProvider.tsx
+++ client/src/app/_components/providers/ReactQueryProvider/ReactQueryProvider.tsx
@@ -1,7 +1,9 @@
 'use client';
 
-import client from '@/utils/general/queryClient';
+import { QueryClient } from '@tanstack/react-query';
 import { QueryClientProvider } from '@tanstack/react-query';
+
+const client = new QueryClient();
 
 export default function ReactQueryProvider({ children }: { children: React.ReactNode }) {
 	return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
